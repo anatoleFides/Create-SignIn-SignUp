f17d38c4c104b54d72e780d6449c199ae40c3251
Anatolii Bedilo <fides2013ml@gmail.com>
2021-12-11 23:53:46 +0300

    add callback for submit. create db.json

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -163,9 +163,30 @@ class SignUp extends HTMLElement {
 				color: #fff; 
 				border: none; 
 				border-radius: 4px; 
-				font-size: 20px; 
+				font-size: 20px;
+				cursor: pointer;
 			`
 		})
+
+		button.onclick = async function (event) {
+			const user = {
+				login: login.value,
+				telephone: telephone.value,
+				email: email.value,
+				password: password.value,
+				avatat: avatar__photo.src
+			}
+			const response = await fetch('http://localhost:3000/users', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(user)
+			})
+			if (response.status === 201) { 
+				document.getElementsByTagName('sign-up')[0].remove() 
+			}
+		}
 	}
 
 	createElem (сontainerName, tagName) { 

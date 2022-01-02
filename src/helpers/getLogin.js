a6b94837948af832cfb2e200dac87c7aa53f94e9
Anatolii Bedilo <fides2013ml@gmail.com>
2022-01-03 00:18:45 +0300

    modifide getLogin.js

--- src/helpers/getLogin.js
+++ src/helpers/getLogin.js
@@ -1,6 +1,5 @@
 import { endpoint } from '../assets'
 
-export const getLogin = async () => {
-	const users = await (await fetch(endpoint)).json()
+export const getLogin = async () => await (await fetch(endpoint)).json()
 
-}
+getLogin().then(response => console.log(response))

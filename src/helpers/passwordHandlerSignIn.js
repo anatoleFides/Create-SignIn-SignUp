959138d07a65d5988bb8d1dfe34a4706b1b4ee1d
Anatolii Bedilo <fides2013ml@gmail.com>
2022-01-11 10:43:19 +0200

    create autorize

--- src/helpers/passwordHandlerSignIn.js
+++ src/helpers/passwordHandlerSignIn.js
@@ -8,13 +8,27 @@ import {
 export const passwordHandlerSignIn = (inputElem) => {
 	const usersLoginPassword = []
 	usersCollection().then(response => response
-		.map(item => usersLoginPassword.push({
-			login: item.login,
-			password: item.password
-		})))
-	
+		.map(item => usersLoginPassword.push(item)))
+		// 	.map(item => usersLoginPassword.push({
+		// 	login: item.login,
+		// 	password: item.password
+		// })))
+		console.log(usersLoginPassword)
+
 	inputElem.oninput = (event) => {
 		const usersPassord = usersLoginPassword.map(item => item.password)
+console.log(usersPassord)
+		const usersLogin = usersLoginPassword.map(item => item.login)
+console.log(usersLogin)
+		const usersTel = usersLoginPassword.map(item => item.telephone)
+console.log(usersTel)
+		const usersEmail = usersLoginPassword.map(item => item.email)
+console.log(usersEmail)
+		const usersAvatar = usersLoginPassword.map(item => item.avatar)
+console.log(usersAvatar)
+
+
+
 
 		const userPasswordIndex = usersPassord.indexOf(usersPassord
 			.filter(item => item === event.target.value)[0])

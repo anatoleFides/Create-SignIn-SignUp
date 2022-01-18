9ebba7b9ebfda06af40376ed04ee2b8cb26f3ac9
Anatolii Bedilo <fides2013ml@gmail.com>
2022-01-18 20:31:06 +0300

    create validete size image

--- src/helpers/readFile.js
+++ src/helpers/readFile.js
@@ -6,12 +6,26 @@ import {
 } from '../styles'
 
 export const readFile = (value) => {
+	console.log(value.files[0])
+	// console.log(value.files[0].type.indexOf('image'))
 	if (value.files[0].type.indexOf('image')) {
-		window[Symbol.for('error__message-avatar')].style = errorMessageActiveStyle
+		Object.assign(window[Symbol.for('error__message-avatar')], {
+			style: errorMessageActiveStyle,
+			textContent: 'Invalid file type'
+		})
 
 		return false
 	} else {
 		window[Symbol.for('error__message-avatar')].style = errorMessageStyle
+		
+		if (value.files[0].size > 90000) {
+			Object.assign(window[Symbol.for('error__message-avatar')], {
+				style: errorMessageActiveStyle,
+				textContent: 'Image size exceeds 90 kB'
+			})
+
+		return false
+		}
 		const reader = new FileReader
 		reader.onload = function (ev) {
 			window[Symbol.for('photo')].src = ev.target.result

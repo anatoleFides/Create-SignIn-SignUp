207bbda7a416cc7cf30c7c3640b7615a661cc1ff
Anatolii Bedilo <fides2013ml@gmail.com>
2022-02-01 00:08:05 +0300

    modifide validate sign up

--- src/helpers/validateEmptyString.js
+++ src/helpers/validateEmptyString.js
@@ -1,8 +1,6 @@
 import { errorMessageStyle } from '../styles'
 
 export const validateEmptyString = (inputElem, property) => {
-
-	console.log(property.length)
 	if (property.length === 0) {
 		inputElem.nextElementSibling.style = errorMessageStyle
 	}

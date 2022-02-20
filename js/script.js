f6fc945e60b5520cb9def2b80a507879718ee7db
Anatolii Bedilo <fides2013ml@gmail.com>
2022-02-20 21:24:52 +0300

    delete file js

--- js/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("singup-btn").onclick = function (event) {
-	event.preventDefault()
-	const customElemSignUp = document.getElementById("sign-up")
-	console.log(customElemSignUp)
-	customElemSignUp.classList.add("open-sign_up")
-}
